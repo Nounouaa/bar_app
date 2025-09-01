d238ed306dafdeefa5ce5fa81778f887cc19d3e0
LOVAMAMY <lovamamy61@gmail.com>
2025-09-01 03:48:07 +0200

    ajout

--- models/vente.js
+++ models/vente.js
@@ -1,11 +1,20 @@
+// models/vente.js
 "use strict";
 module.exports = (sequelize, DataTypes) => {
   const Vente = sequelize.define("Vente", {
-    produit_id: DataTypes.INTEGER,
-    quantite: DataTypes.INTEGER
+    produit_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    quantite: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   }, {});
+
   Vente.associate = function(models) {
     Vente.belongsTo(models.Produit, { foreignKey: "produit_id" });
   };
+
   return Vente;
 };
